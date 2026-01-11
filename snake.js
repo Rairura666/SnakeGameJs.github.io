31d778101fd33721774cae5b56e254e1c9a37763
Rairura <biryukova.ira63@gmail.com>
2026-01-11 18:36:51 +0300

    Snake head moves, the borders of the field teleport the snake.

--- snake.js
+++ snake.js
@@ -1,44 +1,87 @@
 const cellsize = 25
 const rows = 25
-const cols=25
+const cols = 25
+
+let speedX = 0
+let speedY = 0
+let snakeX = 5
+let snakeY = 5
 
 function drawFoodBox(context) {
-    const {x,y} = randomizeFood()
+    const { x, y } = randomizeFood()
     context.fillStyle = "crimson"
-    context.fillRect(x*cellsize,y*cellsize, cellsize, cellsize)
+    context.fillRect(x * cellsize, y * cellsize, cellsize, cellsize)
 }
 
-function drawSnake() {
+function drawSnakeHead(context) {
+    context.fillStyle = "yellow"
+    context.fillRect(snakeX * cellsize, snakeY * cellsize, cellsize, cellsize)
 
+    if (snakeX < 0) {
+        snakeX = cols - 1
+    }
+    if (snakeX > (cols - 1)) {
+        snakeX = 0
+    }
+    if (snakeY < 0) {
+        snakeY = rows - 1
+    } 
+    if (snakeY > (rows - 1)) {
+        snakeY = 0
+    }
 }
 
 function updateBoard(context) {
     context.fillStyle = "blue"
-    context.fillRect(0, 0, board.width, board.height)
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height)
 
-    drawFoodBox(context)
+    snakeX += speedX
+    snakeY += speedY
 
-    
+    // drawFoodBox(context)
+    drawSnakeHead(context)
+
 }
 
 
-function randomizeFood () {
-    const x = Math.floor(Math.random()*(rows-1))+1
-    const y = Math.floor(Math.random()*(cols-1))+1
-    
-    return{x,y}
+function randomizeFood() {
+    const x = Math.floor(Math.random() * (rows - 1)) + 1
+    const y = Math.floor(Math.random() * (cols - 1)) + 1
+
+    return { x, y }
 }
 
+function changeDirection(e) {
+    if (e.code == "ArrowUp") {
+        speedX = 0
+        speedY = -1
+    } else
+        if (e.code == "ArrowDown") {
+            speedX = 0
+            speedY = 1
+        } else
+            if (e.code == "ArrowLeft") {
+                speedX = -1
+                speedY = 0
+            } else
+                if (e.code == "ArrowRight") {
+                    speedX = 1
+                    speedY = 0
+                }
 
+}
 
 window.onload = function () {
-
     const boardElem = document.getElementById("board")
-    boardElem.width = cellsize*cols
-    boardElem.height = cellsize*rows
+    boardElem.width = cellsize * cols
+    boardElem.height = cellsize * rows
     const context = boardElem.getContext("2d")
 
-    updateBoard(context)
+    document.addEventListener("keyup", changeDirection)
+
+    this.setInterval(() => updateBoard(context), 1000 / 10)
+
+
 
 }
 
